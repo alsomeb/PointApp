311cf41b37345504872fbd43df5c74be967b710f
alsomeb <alsomeb@gmail.com>
2022-09-17 12:19:58 +0200

    A little refactoring.

--- src/com/nackademinjava/PointsAnalyzer.java
+++ src/com/nackademinjava/PointsAnalyzer.java
@@ -8,7 +8,9 @@ public class PointsAnalyzer {
 
         // Öppnar en ström på TOTALA points array, filtrerar och leta bara efter negativa punkter
         // sparar dem till en array med dynamisk size på hur många de blev (läs toArray funktion för mer info)
-        return Arrays.stream(totalPoints).filter(point -> point.getY() < 0 && point.getX() < 0).toArray(size -> new Point[size]);
+        return Arrays.stream(totalPoints)
+                .filter(point -> point.getY() < 0 && point.getX() < 0)
+                .toArray(size -> new Point[size]);
     }
 
     public static void printPointArray(Point[] array) {
