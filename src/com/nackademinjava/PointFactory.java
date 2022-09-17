d0a269fa7b167bb1a75932f501478c533c32abd6
alsomeb <alsomeb@gmail.com>
2022-09-17 15:48:33 +0200

    Added method in PointFactory, formattedRandomDouble

--- src/com/nackademinjava/PointFactory.java
+++ src/com/nackademinjava/PointFactory.java
@@ -1,5 +1,7 @@
 package com.nackademinjava;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class PointFactory {
@@ -9,11 +11,18 @@ public class PointFactory {
         return random.nextDouble(-5, 6); // mellan -5.0 till 5.0
     }
 
+    private static double formattedRandomDouble() { // Formatera 1 decimal
+        double randNr = getRandomDouble();
+        BigDecimal formatted = new BigDecimal(randNr).setScale(1, RoundingMode.HALF_UP);
+        return formatted.doubleValue();
+    }
+
     public static Point[] createArrayWithPoints(int amount) {
+
         Point[] punkter = new Point[amount];
 
         for (int i = 0; i < amount; i++) {
-            punkter[i] = new Point(getRandomDouble(), getRandomDouble()); // Skapar points med random X, Y mellan -5.0 till 5.0
+            punkter[i] = new Point(formattedRandomDouble(), formattedRandomDouble()); // Skapar points med random X, Y mellan -5.0 till 5.0
         }
 
         return punkter;
